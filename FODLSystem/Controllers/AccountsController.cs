c0dffdf7d68f973fa8b28bc00fc97739aae74744
Kristoffer Malapit <krismalapit@yahoo.com>
2022-06-02 10:52:58 +0800

    new

--- FODLSystem/Controllers/AccountsController.cs
+++ FODLSystem/Controllers/AccountsController.cs
@@ -37,6 +37,7 @@ namespace FODLSystem.Controllers
         public ActionResult Login(string returnUrl)
         {
             //new updates
+            //delte this
             ViewBag.ReturnUrl = returnUrl;
             if (User.Identity.IsAuthenticated)
             {
